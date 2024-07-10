a588eb659eac747f9936c1071aec42467cf0df60
Narvana <adhikarijagdish3@gmail.com>
2024-07-10 17:01:13 +0530

    Banner Controller  updated

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -23,8 +23,8 @@ class BannerController extends Controller
             'Banner_sub_heading' => $banner ? 'nullable|string|max:50' : 'required|string|max:50',
             'Banner_button_text' => $banner ? 'nullable|string|max:20' : 'required|string|max:20',
             'Banner_image' => $banner 
-                                ? 'nullable|image|mimes:jpeg,png,jpg,gif,svg|min:250|max:5120|dimensions:ratio=180/11' 
-                                : 'required|image|mimes:jpeg,png,jpg,gif,svg|min:250|max:5120|dimensions:ratio=180/11',
+                                ? 'nullable|image|mimes:jpeg,png,jpg,gif,svg|min:250|max:5120' 
+                                : 'required|image|mimes:jpeg,png,jpg,gif,svg|min:250|max:5120',
         ]);
         
         if ($validator->fails()) {
