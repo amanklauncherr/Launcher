28a8f2893c0ac709e4914598b601d743ff420dea
Narvana <adhikarijagdish3@gmail.com>
2024-07-16 16:48:17 +0530

    News Letter Blade Updated

--- resources/views/subscriptionconfirmation.blade.php
+++ resources/views/subscriptionconfirmation.blade.php
@@ -6,10 +6,23 @@
     <title>Subscription Confirmation</title>
 </head>
 <body>
-    <h2>Subscription Confirmation</h2>
-    {{-- <p>Dear {{ $name }},</p> --}}
-    <p>Thank you for subscribing to our newsletter!</p>
-    <p>You are now part of our community. Expect exciting updates and news from us regularly.</p>
-    <p>Best regards,<br>Launcherr Team</p>
+    <p> Subject: 🌍✈️ Welcome to Launcherr! Enjoy 10% Off </p>
+    <p> Hi Subscriber, </p>
+    <p> Welcome to Launcherr! We're thrilled to have you. <strong>Here’s a 10% discount just for you: LAUNCHERRSVX10.</strong> </p>
+    <p> Why Launcherr? </p>
+    <p><strong>Exclusive Deals:</strong>  Offers tailored for you. </p>
+    <p><strong>Inspiring Spots:</strong>   Hidden gems worldwide. </p>
+    <p><strong>Travel Tips:</strong>   Make your trips seamless. </p>
+    <p><br></p>
+    <p> How to Redeem: </p>
+    <p> Visit https://launcherr.co </p>
+    <p> Shop our Travel Sized Products </p>
+    <p>   Use code  <strong>LAUNCHERRSVX10</strong> at checkout.</p>
+    <p><br></p>
+    <p> Let's make your travel dreams come true! </p>
+    <p>
+        Cheers,<br>
+        The Launcherr Team 🌟
+    </p>
 </body>
 </html>
