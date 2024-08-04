f8f306a07e15074d809aec16c57ae07530b3ff31
Narvana <adhikarijagdish3@gmail.com>
2024-08-05 00:09:59 +0530

    Chechk Bearer token add in admin and user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\CheckRole::class,
-        'checkBearerToken' => \App\Http\Middleware\CheckBearerToken::class,
+        'check.bearer.token' => \App\Http\Middleware\CheckBearerToken::class,
         'publictokenOrauth' => \App\Http\Middleware\PublicTokenOrAuth::class,
         // 'checkJwt' => \App\Http\Middleware\CheckJwtToken::class,
     ];
