58234562c8a5fc6c8b877cebd5effddf0f2b90a9
Narvana <adhikarijagdish3@gmail.com>
2024-12-20 12:40:24 +0530

    AddState in State Controller

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -6,10 +6,180 @@ use App\Http\Controllers\Controller;
 use App\Models\state;
 use App\Models\Cites;
 use Illuminate\Http\Request;
+use NunoMaduro\Collision\Adapters\Phpunit\State as PhpunitState;
 
 class StateController extends Controller
 {
-    //
+
+    public function AddStates(Request $request)
+    {
+
+        $json='[
+                {
+                "code": "AN",
+                "name": "Andaman and Nicobar Islands"
+                },
+                {
+                "code": "AP",
+                "name": "Andhra Pradesh"
+                },
+                {
+                "code": "AR",
+                "name": "Arunachal Pradesh"
+                },
+                {
+                "code": "AS",
+                "name": "Assam"
+                },
+                {
+                "code": "BR",
+                "name": "Bihar"
+                },
+                {
+                "code": "CG",
+                "name": "Chandigarh"
+                },
+                {
+                "code": "CH",
+                "name": "Chhattisgarh"
+                },
+                {
+                "code": "DH",
+                "name": "Dadra and Nagar Haveli"
+                },
+                {
+                "code": "DD",
+                "name": "Daman and Diu"
+                },
+                {
+                "code": "DL",
+                "name": "Delhi"
+                },
+                {
+                "code": "GA",
+                "name": "Goa"
+                },
+                {
+                "code": "GJ",
+                "name": "Gujarat"
+                },
+                {
+                "code": "HR",
+                "name": "Haryana"
+                },
+                {
+                "code": "HP",
+                "name": "Himachal Pradesh"
+                },
+                {
+                "code": "JK",
+                "name": "Jammu and Kashmir"
+                },
+                {
+                "code": "JH",
+                "name": "Jharkhand"
+                },
+                {
+                "code": "KA",
+                "name": "Karnataka"
+                },
+                {
+                "code": "KL",
+                "name": "Kerala"
+                },
+                {
+                "code": "LD",
+                "name": "Lakshadweep"
+                },
+                {
+                "code": "MP",
+                "name": "Madhya Pradesh"
+                },
+                {
+                "code": "MH",
+                "name": "Maharashtra"
+                },
+                {
+                "code": "MN",
+                "name": "Manipur"
+                },
+                {
+                "code": "ML",
+                "name": "Meghalaya"
+                },
+                {
+                "code": "MZ",
+                "name": "Mizoram"
+                },
+                {
+                "code": "NL",
+                "name": "Nagaland"
+                },
+                {
+                "code": "OR",
+                "name": "Odisha"
+                },
+                {
+                "code": "PY",
+                "name": "Puducherry"
+                },
+                {
+                "code": "PB",
+                "name": "Punjab"
+                },
+                {
+                "code": "RJ",
+                "name": "Rajasthan"
+                },
+                {
+                "code": "SK",
+                "name": "Sikkim"
+                },
+                {
+                "code": "TN",
+                "name": "Tamil Nadu"
+                },
+                {
+                "code": "TS",
+                "name": "Telangana"
+                },
+                {
+                "code": "TR",
+                "name": "Tripura"
+                },
+                {
+                "code": "UP",
+                "name": "Uttar Pradesh"
+                },
+                {
+                "code": "UK",
+                "name": "Uttarakhand"
+                },
+                {
+                "code": "WB",
+                "name": "West Bengal"
+                }
+                ]';
+
+        $States = json_decode($json,true);
+        
+        $collection = collect($States);
+
+        // Map through the collection to extract name and callingCode
+        $AddStates = $collection->map(function ($States) {
+            return [
+                'state' => $States['name'],
+            ];
+        });
+    
+        // Insert the mapped data into the country_codes table
+        State::insert($AddStates->toArray());
+    
+        return response()->json(['message' => 'States inserted successfully']);
+
+
+    } 
+
     /**
      * @group State&City&Iata
      *
