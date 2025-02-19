41dddeccfa68075c90141a541b2f0320ec4c6a28
Aman kumar <amankumar@launcherr.co>
2025-02-19 19:17:03 +0530

    maintainig both barnch

--- routes/api.php
+++ routes/api.php
@@ -381,7 +381,6 @@ Route::get('getTicket',[DotMikController::class,'printTicket']);
 Route::get('get/pending/Ticket',[DotMikController::class,'saveTicket']);
 
 Route::get('payCash',[CashFreeController::class,'Cashfree_Create_order']);
-
 Route::get('success/Cashfree/{orderId}', [CashFreeController::class, 'PaymentSuccessCashFree'])->name('payment.success');
 
 
